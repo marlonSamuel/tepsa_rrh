2c756bced5400f4286639fb1f3df31218a6603e1
Marlon <marlongf2195@gmail.com>
2020-11-03 22:21:17 -0600

    build_asignacion

--- frontend/src/store/services.js
+++ frontend/src/store/services.js
@@ -23,9 +23,9 @@ import AsistenciaAlmuezoService from '../services/AsistenciaAlmuerzoService'
 import AsignacionDomoService from '../services/AsignacionDomoService'
 
 
-let baseUrl = 'http://www.tepsa-rrh.com/' //base url desarrollo
+//let baseUrl = 'http://www.tepsa-rrh.com/' //base url desarrollo
 
-//let baseUrl = 'https://167.172.158.187/tepsa-rrh/' //url production
+let baseUrl = 'https://167.172.158.187/tepsa-rrh/' //url production
 
 let token_data = $cookies.get('token_data')
 
