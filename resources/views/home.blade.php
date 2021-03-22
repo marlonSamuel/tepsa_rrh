be948c991b347e7899628cb74cd09fe6c5957616
malonSamuel <marlongf2195@gmail.com>
2021-03-22 10:04:25 -0600

    pull build

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -13,6 +13,6 @@
 <body class="hold-transition skin-blue sidebar-mini">
     <div style="height: 100%" id="app"></div>
     <!-- built files will be auto injected -->
-<script type="text/javascript" src="bundle/js/manifest.6aa67b56925988dfee92.js"></script><script type="text/javascript" src="bundle/js/app.75c8baf04f4ce865ef30.js"></script></body>
+<script type="text/javascript" src="bundle/js/manifest.6aa67b56925988dfee92.js"></script><script type="text/javascript" src="bundle/js/app.84caf47259736f2f020d.js"></script></body>
 
 </html>
