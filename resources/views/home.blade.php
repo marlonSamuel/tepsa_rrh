3315fcc8440a65efd7f59945bb2c53dca4410b35
Obed98 <obedzepeda@gmail.com>
2021-04-12 09:51:09 -0600

    correccion error add prestaciones

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -13,6 +13,6 @@
 <body class="hold-transition skin-blue sidebar-mini">
     <div style="height: 100%" id="app"></div>
     <!-- built files will be auto injected -->
-<script type="text/javascript" src="bundle/js/manifest.6aa67b56925988dfee92.js"></script><script type="text/javascript" src="bundle/js/app.5f43439b83ffad54299c.js"></script></body>
+<script type="text/javascript" src="bundle/js/manifest.6aa67b56925988dfee92.js"></script><script type="text/javascript" src="bundle/js/app.524b6eac634b13d5f0c1.js"></script></body>
 
 </html>
